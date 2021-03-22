d8526e714cce292261075cd9c13a269ee90de4c7
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-22 12:16:02 +0000

    restore default redis port

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 def init_redis():
     redis_host = os.getenv("REDIS_HOST", "localhost")
-    redis_port = os.getenv("REDIS_PORT", "55001")
+    redis_port = os.getenv("REDIS_PORT", "6379")
     app.redis_client = redis.Redis(host=redis_host, port=redis_port, db=0)
 
 
