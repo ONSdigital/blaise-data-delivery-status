659d327ad8b69625b386da04ff11eba752b20b35
elthorne <46898565+elthorne@users.noreply.github.com>
2023-12-14 16:23:49 +0000

    wip: catch and print err
    refs: BLAIS5-3756

--- app/batch.py
+++ app/batch.py
@@ -9,8 +9,13 @@ batch = Blueprint("batch", __name__, url_prefix="/v1/batch")
 @batch.route("", methods=["GET"])
 def get_bactches():
     batch = []
-    query = current_app.datastore_client.query(kind=DATASTORE_KIND)
-    result = list(query.fetch())
+    try:
+        query = current_app.datastore_client.query(kind=DATASTORE_KIND)
+        result = list(query.fetch())
+    except Exception as err:
+        print(f"Something didn't work: {err}")
+        raise err
+
     if not result:
         print("No result is returned")
     else:  
