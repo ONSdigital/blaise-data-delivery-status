082b4d33c4ffd4037fb187116256aab5e6de7c98
kristian4res <57638182+kristian4res@users.noreply.github.com>
2023-12-15 11:44:47 +0000

    fix: extend list

--- app/batch.py
+++ app/batch.py
@@ -11,13 +11,12 @@ def get_bactches():
     batch = []
     query = current_app.datastore_client.query(kind=DATASTORE_KIND)
     result = list(query.fetch())
-    if not result:
+    if len(result) == 0:
         print("No result is returned")
     else:  
-        batch.append(result)
+        batch.extend(result)
     return jsonify(batch), 200
 
-# TODO: Verify response object
 @batch.route("/<batch_name>", methods=["GET"])
 def get_batch(batch_name):
     key = current_app.datastore_client.key(DATASTORE_KIND, batch_name)
