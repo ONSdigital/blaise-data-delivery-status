95338d52f3ddc134cc1eb8672f4cbff8ccc4e6e2
elthorne <46898565+elthorne@users.noreply.github.com>
2023-12-18 12:07:40 +0000

    refactor: rename get_bactches() to get_batches()
    refs: BLAIS5-3756

--- app/batch.py
+++ app/batch.py
@@ -7,7 +7,7 @@ batch = Blueprint("batch", __name__, url_prefix="/v1/batch")
 
 
 @batch.route("", methods=["GET"])
-def get_bactches():
+def get_batches():
     batch = []
     query = current_app.datastore_client.query(kind=DATASTORE_KIND)
     result = list(query.fetch())
