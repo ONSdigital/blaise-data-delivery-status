1893c29688e291e3cf7c6f7bcbea6fcb9de4d588
elthorne <46898565+elthorne@users.noreply.github.com>
2023-12-18 11:13:15 +0000

    debug: Determine if print statements or logging will output to GCP
    refs: BLAIS5-3756

--- app/batch.py
+++ app/batch.py
@@ -1,3 +1,5 @@
+import logging
+
 from client.blaise_dds.kind import DATASTORE_KIND
 from flask import Blueprint, current_app, jsonify
 
@@ -8,12 +10,14 @@ batch = Blueprint("batch", __name__, url_prefix="/v1/batch")
 
 @batch.route("", methods=["GET"])
 def get_bactches():
+    print("EL'S DEBUG: Do print statements work?")
+    logging.debug("EL'S DEBUG: Does logging work?")
     batch = []
     query = current_app.datastore_client.query(kind=DATASTORE_KIND)
     result = list(query.fetch())
     if len(result) == 0:
         print("No result is returned")
-    else:  
+    else:
         batch.extend(result)
     return jsonify(batch), 200
 
