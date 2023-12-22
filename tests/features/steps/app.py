7f44865699fb9be8ae31fb89614378b9cfd43115
kristian4res <57638182+kristian4res@users.noreply.github.com>
2023-12-22 17:33:07 +0000

    ref: remove redundant prints; fix query

--- tests/features/steps/app.py
+++ tests/features/steps/app.py
@@ -1,18 +1,13 @@
-from datetime import datetime
 import json
 
-import pytz
 from behave import given, then, when
 from freezegun import freeze_time
 
 
 @given('the current time is set')
 def step_impl(context):
-    print("STEP 2: ", context.time)
     context.freezer = freeze_time(context.time)
     context.freezer.start()
-    print("STEP 2: ", context.time)
-    print("STEP 2: FROME DATETIME ", datetime.now(pytz.utc).replace(microsecond=0).isoformat())
 
 
 @when('I POST to "{path}" with the payload')
@@ -20,7 +15,6 @@ def step_impl(context, path):
     response = context.client.post(
         path, data=context.text, content_type="application/json"
     )
-    print("STEP 3: ", context.time)
     context.response = response.get_json()
     context.response_status_code = response.status_code
     assert True
@@ -51,7 +45,6 @@ def step_impl(context, path):
 
 @then('the response should be')
 def step_impl(context):
-    print("STEP 5: RESPONSE SHOULD BE", context.time)
     context.response["updated_at"] = context.time
 
     context_entity = json.loads(context.text)
@@ -59,16 +52,8 @@ def step_impl(context):
 
     assert context.response == context_entity, f"Response {context.response} did not match expected value: {context.text}"
 
-# @then('the response code should be {status_code}')
-# def step_impl(context, status_code):
-#     assert context.response_status_code == int(status_code), (
-#             f"Response code {context.response_status_code}"
-#             + " did not match expected value: {status_code}"
-#     )
-
 @then('the response code should be "{status_code}"')
 def step_impl(context, status_code):
-    print("STEP 4: RESPONSE CODE STEP", context.time)
     assert context.response_status_code == int(status_code), (
             f"Response code {context.response_status_code}"
             + f" did not match expected value: {status_code}"
--- tests/features/steps/datastore.py
+++ tests/features/steps/datastore.py
@@ -1,6 +1,4 @@
 import json
-from datetime import datetime
-import pytz
 
 from google.cloud import datastore
 from behave import given, then
@@ -9,7 +7,6 @@ from client.blaise_dds import DATASTORE_KIND
 
 @given("Datastore contains")
 def step_impl(context):
-    print("STEP 1:", context.time)
     data = json.loads(context.text)
     if len(data) == 0:
         return None
@@ -20,7 +17,6 @@ def step_impl(context):
         )
         record_entity.update(item)
         record_entity["updated_at"] = context.time
-        print("STEP 1:", context.time)
         context.datastore_client.put(record_entity)
 
 
@@ -28,32 +24,28 @@ def step_impl(context):
 def step_impl(context):
     # arrange
     expected = json.loads(context.text)
-    expected["updated_at"] = datetime.now(pytz.utc).replace(microsecond=0).isoformat()
-
+    expected["updated_at"] = context.time
+    
+    context.datastore_client.wait_for_record_to_be_available(expected["dd_filename"])
     client = context.datastore_client
     query = client.query(kind=DATASTORE_KIND)
-    #query.add_filter("updated_at", "=", expected["updated_at"])
+    query.add_filter("updated_at", "=", expected["updated_at"])
     query_results = list(query.fetch())
 
     # act
     result = dict(list(query_results)[0]) if query_results else None
 
-    print("EXPECTED: ", expected)
-    print("RESULT: ", result)
-    # assert
     assert result == expected, "Resulting Datastore data does not match expected data"
 
 
 @then("Datastore should not contain")
 def step_impl(context):
     # arrange
-    expected = json.loads(context.text)
-
     client = context.datastore_client
     query = client.query(kind=DATASTORE_KIND)
-    query_iter = query.fetch()
+    query_results = list(query.fetch())
 
     # act
-    result = dict(list(query_iter))
+    result = list(query_results) if query_results else []
     # assert
     assert len(result) == 0, "Resulting Datastore data does not match expected data"
